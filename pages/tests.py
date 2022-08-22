b0e20d99666266e21faf91103b293b80002495b9
Camillus83 <kamilrichter98@gmail.com>
2022-08-22 23:08:36 +0200

    homepage upgrade

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from pages.views import HomePageView
+
+
+class HomepageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("home")
+        self.response = self.client.get(url)
+
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, "home.html")
+
+    def test_homepage_correct_html(self):
+        self.assertContains(self.response, "QuizApp Homepage")
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
