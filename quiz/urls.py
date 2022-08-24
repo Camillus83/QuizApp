7f7638a79f3869ed1b9db88a27d7d6aa8d5cc448
Camillus83 <kamilrichter98@gmail.com>
2022-08-24 21:36:39 +0200

    add search do all quizzes

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import QuizListView, QuizDetailView
+from .views import QuizListView, QuizDetailView, SearchResultsListView
 
 urlpatterns = [
     path("", QuizListView.as_view(), name="quiz_list"),
+    path("search/", SearchResultsListView.as_view(), name="search_results"),
     path("<uuid:pk>", QuizDetailView.as_view(), name="quiz_detail"),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Quiz
 from django.views.generic import ListView, DetailView
 
@@ -13,3 +14,15 @@ class QuizDetailView(DetailView):
     model = Quiz
     template_name = "quiz/quiz_detail.html"
     context_object_name = "quiz"
+
+
+class SearchResultsListView(ListView):
+    model = Quiz
+    template_name = "quiz/search_results.html"
+    context_object_name = "quiz_list"
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Quiz.objects.filter(
+            Q(title__icontains=query) | Q(author__username__icontains=query)
+        )
