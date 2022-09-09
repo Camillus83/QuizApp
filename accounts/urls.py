194b900f05e64212ebd682f40595cd5c508afa00
Camillus83 <kamilrichter98@gmail.com>
2022-09-09 08:09:27 +0200

    Added login with GitHub

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import SignUpPageView
+from .views import SignUpPageView, GitHubView
 
 urlpatterns = [
     path("signup/", SignUpPageView.as_view(), name="signup"),
+    path("github/login", GitHubView.as_view(), name="github-view"),
 ]
